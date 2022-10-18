cf5eda22c20e8b74fe9092b5a6b0ccb86a048d7b
Brian Graf <6555031+TheBrianGraf@users.noreply.github.com>
2022-10-18 09:51:50 -0600

    Update Plausible-conf-setup.sh

--- Plausible-conf-setup.sh
+++ Plausible-conf-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 my_ip=$(curl http://checkip.amazonaws.com)
 secret_kb=$(openssl rand -base64 64)
-echo "ADMIN_USER_EMAIL=plausible@briangraf.net" > /var/www/hosting/plausible-conf.env
+echo "ADMIN_USER_EMAIL=plausible@example.com" > /var/www/hosting/plausible-conf.env
 echo "ADMIN_USER_NAME=admin" >> /var/www/hosting/plausible-conf.env
 echo "ADMIN_USER_PWD=ExamplePWD123!" >> /var/www/hosting/plausible-conf.env
 echo "BASE_URL=http://$my_ip" >> /var/www/hosting/plausible-conf.env
